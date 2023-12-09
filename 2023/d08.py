dabaa434afc7374a7f38c096f6bf22d86bc47291
James <j4ln@posteo.net>
2023-12-09 14:19:59 +0000

    add day 8 part 2

--- 2023/d08.py
+++ 2023/d08.py
@@ -1,7 +1,10 @@
-def traverse_route(route, paths, start_node):
+from math import lcm
+
+
+def traverse_route(route, paths, start_node, part2=False):
     node = start_node
     steps = 0
-    while node != "ZZZ":
+    while not node.endswith("Z") if part2 else node != "ZZZ":
         if route[0] == "L":
             node = paths[node][0]
         elif route[0] == "R":
@@ -30,3 +33,10 @@ for path in input[1:]:
 # part 1
 steps = traverse_route(route, paths, "AAA")
 print(steps)  # a: 20513
+
+# part 2
+travel_nodes = [path for path in paths if path.endswith("A")]
+steps = []
+for i, node in enumerate(travel_nodes):
+    steps.append(traverse_route(route, paths, node, True))
+print(lcm(*steps))  # a: 15995167053923
